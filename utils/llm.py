824961a20cf6d0bf54f1abfd147471f0f9d22c89
eshitasapariya <eshita.sapariya@gmail.com>
2026-07-01 07:32:43 +0530

    fixing LLM call

--- utils/llm.py
+++ utils/llm.py
@@ -15,20 +15,9 @@ load_dotenv()
 import os
 from langchain_groq import ChatGroq
 
-# llm = ChatGroq(
-#     model="llama-3.3-70b-versatile",
-#     temperature=0,
-#     api_key=os.getenv("GROQ_API_KEY"),
-# )
-
-from langchain_openai import AzureChatOpenAI
-
-llm = AzureChatOpenAI(
-    model="gpt-4o-mini",
-    api_key="HKFYqFP2TNm86rpPno2N60UbQwHXhMCrS7iKjdpzLfIepWHTxTaxJQQJ99BCACYeBjFXJ3w3AAABACOG8Ajn",
-    azure_endpoint=f"https://apusegtodvoai01.openai.azure.com",
-    api_version="2025-01-01-preview",
-    # temperature=0.001,
-    temperature=0
- 
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0,
+    api_key=os.getenv("GROQ_API_KEY"),
 )
+
