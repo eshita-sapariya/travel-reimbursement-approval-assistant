3e6a0ba057791a40e076f044579a05fa79795bdc
eshitasapariya <eshita.sapariya@gmail.com>
2026-07-01 07:37:31 +0530

    fixing llm initialization

--- utils/llm.py
+++ utils/llm.py
@@ -6,18 +6,9 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-# llm = ChatGoogleGenerativeAI(
-#     model="gemini-2.0-flash",
-#     temperature=0,
-#     google_api_key=os.getenv("GOOGLE_API_KEY")
-# )
-
-import os
-from langchain_groq import ChatGroq
-
-llm = ChatGroq(
-    model="llama-3.3-70b-versatile",
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
     temperature=0,
-    api_key=os.getenv("GROQ_API_KEY"),
+    google_api_key=os.getenv("GOOGLE_API_KEY")
 )
 
